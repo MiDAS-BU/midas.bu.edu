f1b375119e3f1ad5d4386443d7e255f2e9c5dcc3
Manos Athanassoulis <manos.athanassoulis@gmail.com>
2019-05-28 19:14:07 -0400

    minor

---
+++ push-minor-deploy.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+# This scripts commits a minor change and deploys
+
+git pull 
+
+if [ $? -ne 0 ]; then
+    echo "Could not pull. Exiting!"
+    exit
+fi
+
+git add -u && git commit -m "minor"
+if [ $? -ne 0 ]; then
+    echo "Could not commit the minor change. Please check whether your repository tree is consistent. Existing!"
+    exit
+fi
+
+git push
+if [ $? -ne 0 ]; then
+    echo "Could not push. Please check whether your repository tree is consistent. Existing!"
+    exit
+fi
+
+if [ $? -eq 0 ]; then
+    ./deploy.sh
+fi
+
+
+
