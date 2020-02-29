145bcd168cc21e92632879e2c9a18a48a6f2cd14
Dmytro Bogatov <dmytro@dbogatov.org>
2020-02-28 23:35:52 -0500

    Add people and publications.

--- scripts/build.py
+++ scripts/build.py
@@ -12,6 +12,10 @@ def main():
 	from pathlib import Path
 	from jinja2 import Template, Environment, FileSystemLoader
 	from datetime import datetime
+	
+	abspath = os.path.abspath(__file__)
+	dname = os.path.dirname(abspath)
+	os.chdir(f"{dname}/..")
 
 	class DummyFile(object):
 		def write(self, x): pass
@@ -37,12 +41,30 @@ def main():
 	def formatDate(input):
 		date = datetime.strptime(input, "%m/%d/%y")
 		return date.strftime('%A, %b %d')
+		
+	def sortByLastName(input):
+		def sortFunc(person): 
+			names = person["name"].split()
+			return names[len(names) - 1]
+		return sorted(input, key = sortFunc) 
+		
+	def sortByDate(input):
+		def sortFunc(obj): 
+			return datetime.timestamp(datetime.strptime(obj["when"], "%m/%d/%y"))
+		return sorted(input, key = sortFunc)
+	
+	def limit(input, n):
+		return input[:n]
 
 	dist = "./dist"
 	src = "./website"
 
 	templates = Environment(loader=FileSystemLoader(searchpath=str(Path(src) / "templates")))
 	templates.filters['formatDate'] = formatDate
+	templates.filters['sortByLastName'] = sortByLastName
+	templates.filters['sortByDate'] = sortByDate
+	templates.filters['limit'] = limit
+	
 
 	# create sit structure
 	recreateDir(dist, ignore_errors=True)
@@ -52,11 +74,12 @@ def main():
 
 
 	# render templates
-	seminars = loadData("seminars")
 	for path in (Path(src) / "templates").glob('*.html'):
 		if "layout" not in str(path):
 			templates.get_template(path.name).stream(
-				seminars=seminars,
+				seminars=loadData("seminars"),
+				people=loadData("people"),
+				publications=loadData("publications")
 			).dump(str(Path(dist) / path.name))
 
 if __name__ == '__main__':
