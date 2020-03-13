3db3bd471a29d4dc85900b4974681eb26e355de4
Dmytro Bogatov <dmytro@dbogatov.org>
2020-03-12 21:55:41 -0400

    Fix.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -16,14 +16,25 @@ if [ $? -ne 0 ]; then
 	exit
 fi
 
+STATUS="$(git status)"
 
-git subtree push --prefix dist origin gh-pages
-
-if [ $? -ne 0 ]; then
-	echo "Something went wrong"
-	echo "Re-deploying by force deleting gh-page are and re-creating it."
-	./scripts/redeploy.sh -f
+if [[ $STATUS == *"nothing to commit, working directory clean"* ]]
+then
+	sed -i "" '/dist/d' ./.gitignore
+	git add .
+	git commit -m "Edit .gitignore to publish"
+	git push origin `git subtree split --prefix dist master`:gh-pages --force
+	git reset HEAD~
+	git checkout .gitignore
+else
+	echo "Need clean working directory to publish"
 fi
+
+# if [ $? -ne 0 ]; then
+# 	echo "Something went wrong"
+# 	echo "Re-deploying by force deleting gh-page are and re-creating it."
+# 	./scripts/redeploy.sh -f
+# fi
 
 
 
