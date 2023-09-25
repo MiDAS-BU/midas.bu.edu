1727e2680e81be829cf5d8899124101277f22ae2
Manos Athanassoulis <manos.athanassoulis@gmail.com>
2023-09-25 14:52:58 -0400

    adding mark and some periodic updates, on: scripts/build.py website/assets/config/people.yml website/assets/config/publications.yml website/templates/index.html (new files: website/assets/images/people/crovella.png)

--- scripts/build.py
+++ scripts/build.py
@@ -141,7 +141,7 @@ def main():
 	generateClasses(templates)
 
 	# do not regenerate for each page
-	publications = generatePublications("publications", "George_Kollios", "Manos_Athanassoulis", "Evimaria_Terzi", "Charalampos_E._Tsourakakis")
+	publications = generatePublications("publications", "George_Kollios", "Manos_Athanassoulis", "Evimaria_Terzi", "Charalampos_E._Tsourakakis", "Mark_Crovella")
 
 	# render templates
 	for path in (Path(src) / "templates").glob('*.html'):
